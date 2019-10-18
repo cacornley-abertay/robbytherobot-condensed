6069cf9da5eae68544def29105077b105bab2418
Christopher Acornley <chrisacornley@hotmail.co.uk>
2019-10-18 13:29:58 +0100

    Swapped to a more descriptive main.cpp file

--- RobbyTheRobot/main.cpp
+++ RobbyTheRobot/main.cpp
@@ -1,35 +1,31 @@
+// Includes
 #include <iostream>
 #include "GraphicsManager.h"	
 
 //Ponter reference to classes
 Room* room;
 Robot* robby;
-GraphicsManager *picture;
-
+GraphicsManager* picture;
 
 int main()
-
 {
-	room = new Room(5);
-	robby = new Robot();
-	picture = new GraphicsManager(room, robby);
+	room = new Room(5);								// Create a room for Robby to navigate
+	robby = new Robot();							// Create Robby
+	picture = new GraphicsManager(room, robby);		// Create a GraphicsManager to draw everything
 
 	// Speed of the render delay in milliseconds
 	picture->setDrawDelay(1000);
 
-	while (!robby->ahead_is_colour(room, "green"))
-	{
-		if (robby->ahead_is_colour(room, "yellow"))
-		{
-			robby->move();
-			picture->draw();
-		}
-		else
-		{
-			robby->right();
-			picture->draw();
-		}
-	}
-	std::cout << "hello"; //End of Instructions for Robby
-	system ("Pause");
+	/*
+		START OF OUR CODE
+	*/
+
+	// Code here
+
+	/*
+		END OF OUR CODE
+	*/
+
+	std::cout << "End of Instructions";
+	system("Pause");
 }
